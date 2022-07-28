42f4a01bbc0a422daac032424940fca68a249f40
dev1-pcp <mahmudamen@gmail.com>
2022-07-28 22:50:54 +0200

    add score function

--- models/sufficiency_report.py
+++ models/sufficiency_report.py
@@ -17,18 +17,28 @@ class SufficiencyReport(models.Model):
     national_id = fields.Char(string="الرقم القومي",related="emp_id.national_id")
     job_name = fields.Char(string='المسمي الوظيفي',related="emp_id.job_name")
 
-    def _iscore_sum(self):
-        for statement in self.sufficiency_report_line_ids:
-            total = sum([line.amount for line in statement])
-        self.score = total
 
-    score = fields.Float(string= 'درجات العام الحالي' , compute='_iscore_sum')
-    hi_level = fields.Float(string='مرتبة تقرير اداء العام السابق')
+
+
+
+    score = fields.Float(string= 'درجات العام الحالي' ,store=True, readonly=True)
+    hi_level = fields.Float(string='مرتبة تقرير اداء العام السابق' , readonly=True,defualt=0.0)
     course_id = fields.Many2many('hr.course',string='الدورات الحاصل عليها')
     partner_id = fields.Many2one(related='emp_id.parent_id',string='المدير')
     department_id = fields.Many2one(related='emp_id.department_id', string='القسم')
     qualitative_group = fields.Many2one(related='emp_id.qualitative_group', string='المجموعة النوعية')
     sufficiency_report_line_ids = fields.One2many('hr.sufficiency.report.line','report_id')
+
+    @api.onchange('sufficiency_report_line_ids')
+    def _iscore_sum(self):
+        t = 0
+        for statement in self.sufficiency_report_line_ids:
+            total = sum([line.amount for line in statement])
+            t +=total
+            if not total:
+                self.write({'score': t})
+            else:
+                self.write({'score': t})
 
 
 
